c628f2a554627b7262ab506bac297cac0e5109ce
Hendrik Devestel <hendrikdevestel@gmail.com>
2019-05-01 21:22:42 +0200

    add oscilloscoop hint project

---
+++ Oscilloscoop/Oscilloscoop.ino
@@ -0,0 +1,47 @@
+int yellow1 = 4;
+int yellow2 = 5;
+int yellow3 = 6;
+int yellow4 = 7;
+int powerLed = 10;
+int powerSwitch = 13;
+bool delayed = false;
+int delayTime = 4000;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(yellow1, OUTPUT);
+  pinMode(yellow2, OUTPUT);
+  pinMode(yellow3, OUTPUT);
+  pinMode(yellow4, OUTPUT);
+  pinMode(powerLed, OUTPUT);
+  pinMode(powerSwitch, INPUT);
+}
+
+void loop() {
+    if(digitalRead(powerSwitch) == LOW){
+      turnOff();
+    }
+    else{
+      turnOn();
+    }
+}
+
+void turnOn(){
+  digitalWrite(powerLed, HIGH);
+  if(!delayed){
+    delay(delayTime);
+    delayed = true;
+  }
+  digitalWrite(yellow1, HIGH);
+  digitalWrite(yellow2, HIGH);
+  digitalWrite(yellow3, HIGH);
+  digitalWrite(yellow4, HIGH);
+}
+void turnOff(){
+  digitalWrite(powerLed, LOW);
+  digitalWrite(yellow1, LOW);
+  digitalWrite(yellow2, LOW);
+  digitalWrite(yellow3, LOW);
+  digitalWrite(yellow4, LOW);
+  delayed = false;
+}
